0250d53dd8efb149bfb6d7ee0c838f4de8d01b49
DESKTOP-5LKLR5J\DNyaika <davie2086@gmail.com>
2017-12-29 14:19:32 -0600

    pushhing more functionality

--- src/main/java/com/example/demo/Lab3ServerApplication.java
+++ src/main/java/com/example/demo/Lab3ServerApplication.java
@@ -3,9 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-
-@SpringBootApplication
 @EnableConfigServer
+@SpringBootApplication
 public class Lab3ServerApplication {
 
 	public static void main(String[] args) {
